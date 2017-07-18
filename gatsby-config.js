6a5c2209fb615e74754c6bf22da65d373a18c3df
Sam Atkins <sam.atkins@bighealth.com>
2017-07-18 22:18:51 +0100

    chore: Scaffold app

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Sam's Portfolio and Blog`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 690
+            }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`
+        ]
+      }
+    },
+    `gatsby-plugin-sharp`
+  ]
+}
