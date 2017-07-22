73d75d20e5b1af2ddd685b4884f696be8b163a29
Sam <samatkins5@gmail.com>
2017-07-22 20:06:09 +0100

    fix: Nav and Footer no longer duplicated

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,31 +11,33 @@ import '../css/index.css'
 export default function Blog ({ data }) {
   const { edges: posts } = data.allMarkdownRemark
   return (
-    <div className='blog-posts'>
-      {posts
-        .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <div>
-              <Nav />
-              <div className='blog-post-preview' key={post.id}>
-                <h1 className='title'>
-                  <GatsbyLink to={post.frontmatter.path}>
-                    {post.frontmatter.title}
-                  </GatsbyLink>
-                </h1>
-                <h2 className='date'>
-                  {post.frontmatter.date}
-                </h2>
-                <p>
-                  {post.excerpt}
-                </p>
-                <Link to={post.frontmatter.path}>Read more</Link>
+    <div>
+      <Nav />
+      <div className='blog-posts'>
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div>
+                <div className='blog-post-preview' key={post.id}>
+                  <h1 className='title'>
+                    <GatsbyLink to={post.frontmatter.path}>
+                      {post.frontmatter.title}
+                    </GatsbyLink>
+                  </h1>
+                  <h2 className='date'>
+                    {post.frontmatter.date}
+                  </h2>
+                  <p>
+                    {post.excerpt}
+                  </p>
+                  <Link to={post.frontmatter.path}>Read more</Link>
+                </div>
               </div>
-              <Footer />
-            </div>
-          )
-        })}
+            )
+          })}
+      </div>
+      <Footer />
     </div>
   )
 }
