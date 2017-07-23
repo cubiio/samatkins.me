4d10e09d44ba55129cb5d31269a2f23a5c62b1a8
Sam Atkins <sam.atkins@bighealth.com>
2017-07-23 21:22:20 +0100

    feat: Update and styling to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import logo from '../assets/images/logo.svg'
 
 class Header extends Component {
   render () {
     return (
-      <div>
-        <div
-          style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`
-          }}
-        >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `1rem 0.75rem`
-            }}
-          >
-            <h1 style={{ margin: 0, fontSize: `2rem` }}>
+      <div className='header'>
+        <div className='header-inner'>
+          <div>
+            <img className='header-logo' src={logo} alt='Logo image - avatar of the author' />
+            <h1 className='header-title'>
               <Link
                 to='/'
-                style={{
-                  color: 'white',
-                  textDecoration: 'none'
-                }}
               >
-                Sam Atkins: Web Developer
+                Sam Atkins: Full Stack Web Developer
               </Link>
             </h1>
           </div>
