9d709c4be07af1491ad00d4621ca04813de65ea4
Sam Atkins <sam.atkins@bighealth.com>
2017-07-24 21:44:00 +0100

    style: Update styling for Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,9 @@ import SkillsOverview from '../components/SkillsOverview'
 class Hero extends Component {
   render () {
     return (
-      <div className='hero__wrapper'>
-        <h2 className='hero__tagline'>Dad. Husband. Web developer.</h2>
-        <div className='hero__intro'>
+      <div className='hero-wrapper'>
+        <h2 className='hero-tagline'>Dad. Husband. Web developer.</h2>
+        <div className='hero-intro'>
           <p>I’m a Junior Full-Stack Developer at <a href='https://www.bighealth.com/'>Big Health</a>.</p>
         </div>
         <SkillsOverview />
