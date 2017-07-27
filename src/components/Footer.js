7afde4a3145b8ffae4384bd35df211343ab1595c
Sam Atkins <sam.atkins@bighealth.com>
2017-07-27 07:28:27 +0100

    style: Updates for Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render () {
     return (
-      <div className='footer__wrapper'>
-        <div className='footer__container'>
-          <p className='footer-text'>Built with ♥ by Sam. View source <a href='https://github.com/cubiio/samatkins.me'>code</a>. © 2016-2017 Sam Atkins</p>
+      <div className='footer-wrapper'>
+        <div className='footer-container'>
+          <p>Built with ♥ by Sam. View source <a href='https://github.com/cubiio/samatkins.me'>code</a>. © 2016-2017 Sam Atkins</p>
         </div>
       </div>
     )
