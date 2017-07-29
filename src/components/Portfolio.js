ec8780051611a33fd3d2bbfd3c36b66e16842d39
Sam Atkins <samatkins5@gmail.com>
2017-07-29 10:16:40 +0100

    feat: Basic wired up Component with state
    Pulls data from `projects.json`

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+const portfolio = require('../../data/projects.json')
 
 class Portfolio extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      portfolio
+    }
+  }
+
+  renderProject () {
+    const project = this.state.portfolio
+    return project.map(p =>
+      <div key={p.portfolioTitle} p={p}>{p.portfolioTitle}</div>
+    )
+  }
+
   render () {
     return (
-      <div>Hello world!</div>
+      <div>
+        {this.renderProject()}
+      </div>
     )
   }
 }
