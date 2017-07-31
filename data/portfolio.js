5e7c0170dd7328d68c2b4451453f3198c205f42b
Sam Atkins <samatkins5@gmail.com>
2017-07-31 08:51:14 +0100

    data: Remove JSON, switch to js file with require for img

--- data/portfolio.js
+++ data/portfolio.js
@@ -10,12 +10,66 @@ const portfolio = [
   },
   {
     id: 1,
+    portfolioImage: require('../assets/images/pomodoro_medium.png'),
+    portfolioAlt: 'Pomodoro Clock App',
+    portfolioTitle: 'Pomodoro Clock App',
+    portfolioSnippet: 'JavaScript | React.js',
+    portfolioSourceURL: 'https://github.com/cubiio/pomodoro-app',
+    portfolioSummary: 'Built with React.js and JavaScript, the user can increase or decrease the timers. After each timer elapses sound plays to alert the user.'
+  },
+  {
+    id: 2,
     portfolioImage: require('../assets/images/markdown-previewer_medium.png'),
     portfolioAlt: 'Markdown Previewer App',
     portfolioTitle: 'Markdown Previewer App',
     portfolioSnippet: 'JavaScript | React.js',
     portfolioSourceURL: 'https://github.com/cubiio/react-markdownPreviewer',
     portfolioSummary: 'Built with React.js, user typed markdown is rendered in HTML in the preview pane.'
+  },
+  {
+    id: 3,
+    'portfolioImage': require('../assets/images/item_catalog_medium.png'),
+    portfolioAlt: 'Udacity - Item Catalogue App',
+    portfolioTitle: 'Item Catalogue App',
+    portfolioSnippet: 'Python | Flask | SQLAlchemy | Jinja2',
+    portfolioSourceURL: 'https://github.com/cubiio/fsnd-item_catalog',
+    portfolioSummary: 'Built with Python Flask, SQLAlchemy, Jinja2 and Bootstrap. Functionality includes OAuth v2.0 integration for Google accounts, and CRUD with CSRF protection for entries and local permission systems.'
+  },
+  {
+    id: 4,
+    portfolioImage: require('../assets/images/python_blog_medium.png'),
+    portfolioAlt: 'Udacity - Blog App',
+    portfolioTitle: 'Blog App',
+    portfolioSnippet: 'Python | Google App Engine | Jinja2',
+    portfolioSourceURL: 'https://github.com/cubiio/fsnd-blog',
+    portfolioSummary: 'Built with Python, Google App Engine, and Jinja2. Functionality includes user access controls with signup, login and logout, and password hashing. CRUD local permission systems for posts, comments and likes.'
+  },
+  {
+    id: 5,
+    portfolioImage: require('../assets/images/wikipediaViewer_medium.png'),
+    portfolioAlt: 'Wikipedia Viewer App',
+    portfolioTitle: 'Wikipedia Viewer App',
+    portfolioSnippet: 'Javascript | jQuery | 3rd Party API',
+    portfolioSourceURL: 'https://github.com/cubiio/fccfe-wikipediaViewer',
+    portfolioSummary: 'Built with Javascript, HTML and CSS. Based on user input a call is made to Wikipedia API and search results displayed in the app.'
+  },
+  {
+    id: 6,
+    portfolioImage: require('../assets/images/localWeather_medium.png'),
+    portfolioAlt: 'Local Weather App',
+    portfolioTitle: 'Local Weather App',
+    portfolioSnippet: 'Javascript | jQuery | 3rd Party API',
+    portfolioSourceURL: 'https://github.com/cubiio/fccfe-localWeather',
+    portfolioSummary: 'Built with Javascript and jQuery. An API call to DarkSky returns weather local to the user.'
+  },
+  {
+    id: 7,
+    portfolioImage: require('../assets/images/drum-machine_medium.png'),
+    portfolioAlt: 'JavaScript Drum Machine',
+    portfolioTitle: 'JavaScript Drum Machine',
+    portfolioSnippet: 'Javascript',
+    portfolioSourceURL: 'https://github.com/cubiio/js-drum-machine',
+    portfolioSummary: 'Built with Javascript. A simple drum machine, hit a key and a drum sound plays.'
   }
 ]
 
