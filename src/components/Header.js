5a0ea2b765ec6a43fdb87617fa54b36257095958
Sam Atkins <samatkins5@gmail.com>
2017-08-01 22:21:59 +0100

    style: Header styles use styled.component
    Also includes theme/colours file, and typography.js options file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,76 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
-import images from '../../assets/images'
+import styled from 'styled-components'
+
+import { logo } from '../../assets/images'
+import { HEADER_SHADOW, TITLE_TEXT_COLOR } from '../lib/theme/colours'
 
 class Header extends Component {
   render () {
     return (
-      <div className='header'>
-        <div className='header-inner'>
+      <HeaderWrapper>
+        <HeaderInner>
           <div>
             <Link to='/'>
-              <img className='header-logo' src={images.logo.header} alt='Logo image - avatar of the author'
+              <HeaderLogo
+                src={logo.avatar} alt='Logo image - avatar of the author'
               />
             </Link>
-            <h1 className='header-title'>
+            <HeaderTitle>
               <Link
                 to='/'
               >
                 Sam Atkins: Full Stack Web Developer
               </Link>
-            </h1>
+            </HeaderTitle>
           </div>
-        </div>
-      </div>
+        </HeaderInner>
+      </HeaderWrapper>
     )
   }
 }
 
 export default Header
+
+/*
+Styles
+ */
+
+const HeaderWrapper = styled.div`
+  box-shadow: 0 2px 5px ${HEADER_SHADOW};
+  min-height: 56px;
+  transition: min-height .3s;
+  width: 100%;
+`
+
+const HeaderInner = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+`
+
+const HeaderLogo = styled.img`
+  border-radius: 50px;
+  height: 72px;
+  margin-bottom: 1em;
+  margin-left: 1em;
+  margin-right: 1em;
+  margin-top: 1em;
+  vertical-align: top;
+`
+
+const HeaderTitle = styled.h1`
+  display: inline-block;
+  font-family: $title-font;
+  font-size: 2.5em;
+  font-weight: 300;
+  margin: .75em .25em;
+
+  a {
+    color: ${TITLE_TEXT_COLOR};
+    text-decoration: none;
+  }
+`
