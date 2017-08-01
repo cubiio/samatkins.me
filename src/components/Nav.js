36029084b54d844b4ffa090c43cd45761ca99244
Sam Atkins <samatkins5@gmail.com>
2017-08-01 22:37:16 +0100

    style:  Component uses styled.component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
-// /TODO remove <li>
+import styled from 'styled-components'
+import { NAV_ANCHOR } from '../lib/theme/colours'
+
 class Nav extends Component {
   render () {
     return (
-      <nav className='nav'>
-        <ul className='nav-list'>
-          <li className='nav-item'>
-            <Link className='nav-anchor' to='/'>Home</Link>
-          </li>
-          <li className='nav-item'>
-            <Link className='nav-anchor' to='/portfolio'>Portfolio</Link>
-          </li>
-          <li className='nav-item'>
-            <Link className='nav-anchor' to='/blog'>Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      <NavWrapper>
+        <NavList>
+          <NavItem>
+            <NavAnchor to='/'>Home</NavAnchor>
+          </NavItem>
+          <NavItem>
+            <NavAnchor to='/portfolio'>Portfolio</NavAnchor>
+          </NavItem>
+          <NavItem>
+            <NavAnchor to='/blog'>Blog</NavAnchor>
+          </NavItem>
+        </NavList>
+      </NavWrapper>
     )
   }
 }
 
 export default Nav
+
+/*
+Styles``
+ */
+
+const NavWrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+`
+
+const NavAnchor = styled(Link)`
+  color: ${NAV_ANCHOR};
+  text-decoration: none;
+`
+
+const NavList = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  margin: 0;
+  max-width: 100%;
+  padding: 0;
+`
+
+const NavItem = styled.li`
+  box-sizing: border-box;
+  display: flex;
+  display: inline-block;
+  flex: 1;
+  line-height: 24px;
+  padding: 10px;
+  text-align: center;
+  text-transform: uppercase;
+  width: 20%;
+`
