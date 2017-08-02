daf57b5a2bd262516ee836b1b3cebb9942779740
Sam Atkins <samatkins5@gmail.com>
2017-08-02 22:15:59 +0100

    style: Move to styled component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react'
 import SkillsOverview from '../components/SkillsOverview'
+import styled from 'styled-components'
 
 class Hero extends Component {
   render () {
     return (
-      <div className='hero-wrapper'>
-        <h2 className='hero-tagline'>Dad. Husband. Web developer.</h2>
-        <div className='hero-intro'>
+      <HeroWrapper>
+        <HeroTagline>Dad. Husband. Web developer.</HeroTagline>
+        <HeroIntro>
           <p>I’m a Junior Full-Stack Developer at <a href='https://www.bighealth.com/'>Big Health</a>.</p>
-        </div>
+        </HeroIntro>
         <SkillsOverview />
-      </div>
+      </HeroWrapper>
     )
   }
 }
 
 export default Hero
+
+/*
+Styles
+ */
+
+const HeroWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`
+
+const HeroTagline = styled.h2`
+  align-self: center;
+  color: #212121;
+  display: flex;
+  font-family: $general-font;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const HeroIntro = styled.div`
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 45%;
+
+  a {
+    color: $nav-anchor-color;
+    text-decoration: none;
+  }
+`
