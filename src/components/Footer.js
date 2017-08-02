330fe843c48ef7c65c0b703ccb7bb37858ec1806
Sam Atkins <samatkins5@gmail.com>
2017-08-02 22:27:23 +0100

    style: Move to styled component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import { NAV_ANCHOR } from '../lib/theme/colours'
 
 class Footer extends Component {
   render () {
     return (
-      <div className='footer-wrapper'>
-        <div className='footer-container'>
+      <FooterWrapper>
+        <FooterContainer>
           <p>Built with ♥ by Sam. View source <a href='https://github.com/cubiio/samatkins.me'>code</a>. © 2016-2017 Sam Atkins</p>
-        </div>
-      </div>
+        </FooterContainer>
+      </FooterWrapper>
     )
   }
 }
 
 export default Footer
+
+/*
+Styles
+ */
+
+const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1em;
+`
+
+const FooterContainer = styled.div`
+  display: flex;
+  font-size: 1.2em;
+  justify-content: center;
+  padding-top: 1em;
+
+  a {
+    color: ${NAV_ANCHOR};
+    text-decoration: none;
+  }
+`
--- src/components/SMIcons.js
+++ src/components/SMIcons.js
@@ -1,26 +1,49 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import images from '../../assets/images'
 
 const SMIcons = () => {
   return (
-    <div className='social-container'>
-      <div className='social-logo'>
+    <SocialContainer>
+      <SocialLogo>
         <a href='https://github.com/cubiio' target='_blank'>
           <img src={images.fa.github} />
         </a>
-      </div>
-      <div className='social-logo'>
+      </SocialLogo>
+      <SocialLogo>
         <a href='https://www.linkedin.com/in/atkinssam/' target='_blank'>
           <img src={images.fa.linkedin} />
         </a>
-      </div>
-      <div className='social-logo'>
+      </SocialLogo>
+      <SocialLogo>
         <a href='https://twitter.com/cubiio' target='_blank'>
           <img src={images.fa.twitter} />
         </a>
-      </div>
-    </div>
+      </SocialLogo>
+    </SocialContainer>
   )
 }
 
 export default SMIcons
+
+/*
+Styles
+ */
+
+const SocialContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 2.5em;
+  width: 25%;
+`
+
+const SocialLogo = styled.div`
+  height: 30px;
+  padding-left: 2px;
+  padding-right: 2px;
+  width: 30px;
+`
