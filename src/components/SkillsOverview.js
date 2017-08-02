d99b7d5d6724f89e68419c069255f31c3f9f4adf
Sam Atkins <samatkins5@gmail.com>
2017-08-02 22:16:16 +0100

    style: Move to styled component

--- src/components/SkillsOverview.js
+++ src/components/SkillsOverview.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
 import SkillList from './SkillList'
 const skills = require('../../data/skills.json')
 
@@ -16,12 +18,42 @@ class SkillsOverview extends Component {
 
   render () {
     return (
-      <div className='skills-section'>
-        <h2 className='skills-header'>Skills and Experience</h2>
-        <div className='skills-wrapper'>{this.renderSkills()}</div>
-      </div>
+      <SkillsSection>
+        <SkillsHeader>Skills and Experience</SkillsHeader>
+        <SkillsWrapper>{this.renderSkills()}</SkillsWrapper>
+      </SkillsSection>
     )
   }
 }
 
 export default SkillsOverview
+
+/*
+Styles
+ */
+
+const SkillsSection = styled.div`
+  border-radius: 6px;
+  font-size: 18px;
+  font-weight: 200;
+  line-height: 30px;
+  margin: 50px auto 0;
+  padding: 5px;
+  width: 90%;
+`
+
+// TODO use scale ratio or rythm unit instead of font size
+const SkillsHeader = styled.h2`
+  font-size: 30px;
+  padding-top: 10px;
+  text-align: center;
+`
+
+// TODO Add below to @media query row for large screens; column for small screens
+const SkillsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  font-family: $general-font;
+  justify-content: space-around;
+`
