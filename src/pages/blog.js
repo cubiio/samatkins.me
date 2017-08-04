eca4c0d4c68903fc0094e1e799fced1a2545a1a5
Sam Atkins <samatkins5@gmail.com>
2017-08-04 18:09:12 +0100

    feat: Change layout, animation, and formatting of blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,21 +1,24 @@
 /* global graphql */
 
 import React from 'react'
-import GatsbyLink from 'gatsby-link'
+import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import SMIcons from '../components/SMIcons'
-import Link from '../components/Link'
-import { NAV_ANCHOR, NAV_ANCHOR_HOVER } from '../lib/theme/colours'
+import {
+  BACKGROUND_BLOGPOST_HOVER,
+  NAV_ANCHOR,
+  NAV_ANCHOR_HOVER
+} from '../lib/theme/colours'
 
 export default function Blog ({ data }) {
   const { edges: posts } = data.allMarkdownRemark
   return (
     <div>
       <Nav />
-      <div className='blog-post'>
+      <BlogPostWrapper>
         {posts
           .filter(post => post.node.frontmatter.title.length > 0)
           .map(({ node: post }) => {
@@ -23,22 +26,15 @@ export default function Blog ({ data }) {
               <div>
                 <BlogPostPreview key={post.id}>
                   <BlogPostTitle>
-                    <GatsbyLink to={post.frontmatter.path}>
-                      {post.frontmatter.title}
-                    </GatsbyLink>
+                    <Link to={post.frontmatter.path}>
+                      {post.frontmatter.title} / published {post.frontmatter.date}
+                    </Link>
                   </BlogPostTitle>
-                  <BlogPostDate className='date'>
-                    {post.frontmatter.date}
-                  </BlogPostDate>
-                  <BlogPostExcerpt>
-                    {post.excerpt}
-                  </BlogPostExcerpt>
-                  <Link to={post.frontmatter.path}>Read more</Link>
                 </BlogPostPreview>
               </div>
             )
           })}
-      </div>
+      </BlogPostWrapper>
       <SMIcons />
       <Footer />
     </div>
@@ -58,7 +54,7 @@ export const pageQuery = graphql`
           id
           frontmatter {
             title
-            date(formatString: "DD MMMM YYYY")
+            date(formatString: "DD-MMM-YYYY")
             path
           }
         }
@@ -71,38 +67,36 @@ export const pageQuery = graphql`
 Styles
  */
 
-// const BlogPost =styled.div`
-//
-// `
-
-const BlogPostPreview = styled.div`
+const BlogPostWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  padding: 1rem 0.25rem;
+`
+
+const BlogPostPreview = styled.div`
+  padding: .5rem 0.25rem;
   border-bottom: 2px solid #EEE;
   margin: 0 auto;
 `
 
-const BlogPostTitle = styled.h1`
+const BlogPostTitle = styled.div`
+  flex-basis: auto;
+  align-self: flex-start;
+  font-weight: bold;
+  margin: 0;
+  position: relative;
+  padding: 15px 10px;
+
   a {
     color: ${NAV_ANCHOR};
     text-decoration: none;
+    padding: 0 0 12;
+    display: block;
+    transition: all 0.2s ease-in-out;
   }
 
-  a:hover {
-    color: ${NAV_ANCHOR_HOVER};
-  }
-`
-
-// TODO change font size
-const BlogPostDate = styled.h2`
-  color: ${NAV_ANCHOR};
-  font-size: 1.3em;
-  margin-bottom: 1rem;
-`
-
-const BlogPostExcerpt = styled.p`
-  font-style: italic;
+    &:hover {
+      color: ${NAV_ANCHOR_HOVER};
+      background: ${BACKGROUND_BLOGPOST_HOVER};
+      padding: 25px 15px;
+    }
 `
