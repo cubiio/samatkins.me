13ee03a4ecdd82611721831244187946085dd6f2
Sam Atkins <samatkins5@gmail.com>
2017-08-05 22:37:59 +0100

    style: Initial styles for blogpost tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-import '../styles/main.scss'
+import { NAV_ANCHOR, NAV_ANCHOR_HOVER } from '../lib/theme/colours'
 
 export default function Tags ({ list = [] }) {
   return (
-    <ul className='tag-list'>
+    <TagListWrapper>
+      Tags:
       {list.map(tag =>
-        <li key={tag}>
+        <TagList key={tag}>
           <Link to={`/tags/${tag}`}>
             {tag}
           </Link>
-        </li>
+        </TagList>
       )}
-    </ul>
+    </TagListWrapper>
   )
 }
+
+/*
+Styles
+ */
+
+const TagListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: 50px;
+`
+
+const TagList = styled.div`
+  padding-left: 15px;
+  padding-right: 2px;
+  a {
+    color: ${NAV_ANCHOR};
+    text-decoration: none;
+    border-color: red;
+    border-width: medium;
+  }
+
+  a:hover {
+    color: ${NAV_ANCHOR_HOVER};
+  }
+`
