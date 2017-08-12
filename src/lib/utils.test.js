4343cc00372a1a1ddc7a4fb4fa97b08057108e83
Sam Atkins <samatkins5@gmail.com>
2017-08-12 08:56:06 +0100

    feat: Helper function to format dates, with unit test

--- src/lib/utils.test.js
+++ src/lib/utils.test.js
@@ -7,12 +7,12 @@ const convertFrontMatterDate = require('./utils')
  */
 
 describe('convertFrontMatterDate', () => {
-  it('should return a human readable date', () => {
+  it('should return a formatted date', () => {
     const testDate = '2017-07-18T13:35:13.234Z'
     const outputDate = '18-Jul-2017'
-    // const expectedDate = expect.stringContaining('18-Jul-2017')
+    const testDate2 = '2017-10-09T03:49:16.408Z'
+    const outputDate2 = '9-Oct-2017'
     expect(convertFrontMatterDate(testDate)).toMatch(outputDate)
+    expect(convertFrontMatterDate(testDate2)).toMatch(outputDate2)
   })
 })
-
-// 2017-07-14T03:49:16.408Z
