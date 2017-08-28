2ee0373e16497a01632c652d06393e27c5e35e39
Sam Atkins <samatkins5@gmail.com>
2017-08-28 09:44:29 +0100

    style: update copy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,10 +11,27 @@ class Hero extends Component {
       <HeroWrapper>
         <HeroTagline>Dad. Husband. Web developer.</HeroTagline>
         <HeroIntro>
-          <p>I live in London with my family, and I work at <a href='https://www.bighealth.com/'>Big Health</a> as a Junior Full-Stack Developer. I work across front-end and back-end technologies but my passion is front-end, and I currently work and play around with React and React Native.</p>
-          <p>My background is in supply chain and project management, and I worked for over nine years in Big Four management consulting.</p>
-          <p>Outside of work, I’m a family man and I like cycling, mountain biking, football and rugby.</p>
-          <p>This is my personal website, portfolio, and blog. I also list the technologies I use and want to learn below. Apologies for the lack of design on the site, this is due to time constraints but mainly because whilst I want to learn more about good design, I will always be a developer rather than a designer.</p>
+          <p>
+            I live in London with my family, and I'm a Full-Stack Engineer at{' '}
+            <a href='https://www.bighealth.com/'>Big Health</a>. I work across
+            front-end and back-end technologies but my passion is front-end, and
+            I currently work and play around with React and React Native.
+          </p>
+          <p>
+            My background is in supply chain and project management, and I
+            worked for over nine years in Big Four management consulting.
+          </p>
+          <p>
+            Outside of work, I’m a family man and I like cycling, mountain
+            biking, football and rugby.
+          </p>
+          <p>
+            This is my personal website, portfolio, and blog. I also list the
+            technologies I use and want to learn below. Apologies for the lack
+            of design on the site, this is due to time constraints but mainly
+            because whilst I want to learn more about good design, I will always
+            be a developer rather than a designer.
+          </p>
         </HeroIntro>
         <HeroLists />
       </HeroWrapper>
