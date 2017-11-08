4d8705fc56a8c3d1ce6900f9585d2276d38f0d1e
Sam Atkins <samatkins5@gmail.com>
2017-11-08 18:49:57 +0000

    chore: Add Twitter plugin, and lint file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
-const config = require('./data/siteConfig')
+const config = require('./data/siteConfig');
 
 module.exports = {
   siteMetadata: {
     author: config.siteAuthor,
-    title: config.siteTitle
+    title: config.siteTitle,
   },
   plugins: [
     'gatsby-plugin-catch-links',
@@ -11,44 +11,45 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages/blog`,
-        name: 'pages'
-      }
+        name: 'pages',
+      },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 690
-            }
+              maxWidth: 690,
+            },
           },
           {
-            resolve: `gatsby-remark-responsive-iframe`
+            resolve: 'gatsby-remark-responsive-iframe',
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`
-        ]
-      }
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-twitter',
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/lib/theme/typography.js`
-      }
+        pathToConfigModule: 'src/lib/theme/typography.js',
+      },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: config.googleAnalyticsID
-      }
-    }
-  ]
-}
+        trackingId: config.googleAnalyticsID,
+      },
+    },
+  ],
+};
