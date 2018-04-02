903469f04fdaeb104dfb7e5d25e1984a9f6f6c2c
Sam Atkins <samatkins@outlook.com>
2018-04-02 16:43:12 +0100

    remove query for another day

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,53 +73,6 @@ module.exports = {
     },
     {
       resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
-            }
-          }
-        }
-      `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) =>
-              allMarkdownRemark.edges.map(edge =>
-                Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                })
-              ),
-            query: `
-            {
-              allMarkdownRemark(
-                limit: 1000,
-                sort: { order: DESC, frontmatter: [frontmatter___date] },
-              ) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    frontmatter {
-                      title
-                      date
-                    }
-                  }
-                }
-              }
-            }
-          `,
-            output: '/rss.xml',
-          },
-        ],
-      },
     },
   ],
 };
