3febeed4780b1426a82aff33ddda5056c008bd8c
Sam Atkins <samatkins@outlook.com>
2018-04-15 17:32:38 +0100

    style: Update hero text copy languages

--- src/components/HeroTextSwitcher/heroTextLanguages.js
+++ src/components/HeroTextSwitcher/heroTextLanguages.js
@@ -1,16 +1,20 @@
 export const heroTextEnglish = {
-  greeting: 'Hi, I\'m Sam',
+  greeting: 'Hi, I\'m Sam.',
   textBody:
     'I live with my family in London. I\'m a Software Engineer at Big Health' +
-    ', and I enjoy doing work that matters.',
+    ' and I enjoy doing work that matters.',
 };
 
 export const heroTextGerman = {
-  greeting: 'Hi, Ich bin Sam',
-  textBody: 'Ich wohne in London',
+  greeting: 'Hi, ich bin Sam.',
+  textBody:
+    'Ich wohne in London mit meiner Familie. Ich bin Software Ingenieur bei ' +
+    'Big Health und ich mache gerne Arbeit, die zählt.',
 };
 
 export const heroTextPolish = {
-  greeting: 'Czesc, jestem Sam',
-  textBody: 'Mieszkam w Londonia',
+  greeting: 'Cześć, jestem Sam.',
+  textBody:
+    'Mieszkam w Londynie z moją rodziną. Pracuję jako inżynier oprogramowania' +
+    ' w Big Health. Lubię robić to, co ważne.',
 };
